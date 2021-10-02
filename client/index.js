c11a68555a955853eddbb893989963afa7a036a5
Ashley Williams <ashleyswilliams01@gmail.com>
2021-10-02 15:08:34 -0600

    persist store: zip code state persistent across refresh

--- client/index.js
+++ client/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { Router } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import history from './history'
-import store from './store'
+import { store, persistor} from './store'
 import App from './App'
+import Loading from './components/Loading';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>,
    </Provider>,
   document.getElementById('app')
--- client/store/index.js
+++ client/store/index.js
@@ -1,17 +1,26 @@
 
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-// import auth from './auth'
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+// import auth from './auth';
 import zip from './zip';
 
-const reducer = combineReducers({ zip })
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+}
+
+const pReducer = persistReducer(persistConfig, combineReducers({ zip }))
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
-const store = createStore(reducer, middleware)
+export const store = createStore(pReducer, middleware)
+export const persistor = persistStore(store)
 
-export default store
 // export * from './auth'
 
